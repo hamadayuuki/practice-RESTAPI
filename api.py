dd3d575d769c069a0eb37efa0b6073cbf4db5c43
hamadayuuki <yuukirinnma@gmail.com>
2023-06-01 14:49:18 +0900

    delete: パスワードのハッシュ化を無効化

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from passlib.context import CryptContext
 
 app = FastAPI()
 users_db = {}
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")   # パスワードのハッシュ化
+#pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")   # パスワードのハッシュ化
 
 class UserIn(BaseModel):
     user_id: str = Field(..., min_length=6, max_length=20, regex="^[a-zA-Z0-9_-]+$")
@@ -25,6 +25,8 @@ class ErrorResponse(BaseModel):
 async def signup(user: UserIn):
     user_id = user.user_id
     password = user.password
+    print(user_id)
+    print(password)
     
     # ユーザー登録済みの場合
     if user_id in users_db:
@@ -34,8 +36,8 @@ async def signup(user: UserIn):
         ), 400
 
     try:
-        hashed_password = pwd_context.hash(password)   # パスワードをハッシュ化
-        users_db[user_id] = {"nickname": user_id, "password": hashed_password}   # DBへ格納
+        #hashed_password = pwd_context.hash(password)   # パスワードをハッシュ化
+        users_db[user_id] = {"nickname": user_id, "password": password}   # DBへ格納
         return {"user_id": user_id, "nickname": user_id}
     except ValidationError as e:
         return ErrorResponse(
